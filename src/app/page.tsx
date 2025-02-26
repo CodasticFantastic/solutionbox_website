f8690519dde604c3a482c616a52eaf3b4c9f413d
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-02-26 15:21:41 +0100

    Typho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import OurServicesSection from "@/components/sections/presentableSections/ourServicesSection/OurServicesSection";
 import WhyUsSection from "@/components/sections/presentableSections/whyUsSection/WhyUsSection";
 import HomePageHeroSection from "@/components/sections/presentableSections/homePageHeroSection/HomePageHeroSection";
-import DeliveringNewestTechSolutionsSection from "@/components/sections/presentableSections/deliveringNewestTechSolutionsSectionn/DeliveringNewestTechSolutionsSection";
+import DeliveringNewestTechSolutionsSection from "@/components/sections/presentableSections/deliveringNewestTechSolutionsSection/DeliveringNewestTechSolutionsSection";
 
 export default function Home() {
   return (
