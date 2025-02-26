834320399c420ce978d3ee0ba838071f3994caf0
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-02-26 11:42:53 +0100

    Added 2nd section to Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HomePage2ndSection from "@/pages/homePage/HomePage2ndSection";
 import HomePageHeroSection from "@/pages/homePage/HomePageHeroSection";
 
 export default function Home() {
   return (
     <main>
       <HomePageHeroSection />
+      <HomePage2ndSection />
     </main>
   );
 }
