06dafeea3c3b53a645f91f67986d90a80c80804c
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-03-13 09:53:59 +0100

    Img Cors Config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,33 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "solutionbox.pl", "nowe.solutionbox.pl"],
+    remotePatterns:
+      process.env.NODE_ENV !== "production"
+        ? [
+            {
+              protocol: "http",
+              hostname: "localhost",
+              port: "3000",
+              pathname: "/api/files/**",
+              search: "",
+            },
+          ]
+        : [
+            {
+              protocol: "https",
+              hostname: "solutionbox.pl",
+              port: "",
+              pathname: "/api/files/**",
+              search: "",
+            },
+            {
+              protocol: "https",
+              hostname: "nowe.solutionbox.pl",
+              port: "",
+              pathname: "/api/files/**",
+              search: "",
+            },
+          ],
   },
 };
 
